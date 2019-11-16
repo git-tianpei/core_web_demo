02a8e4ebb245a5bdabcb9caa7941979ebd9af333
wangtp <tp.wong@foxmail.com>
2019-11-16 13:15:03 +0800

    fixbuild

--- build.sh
+++ build.sh
@@ -6,22 +6,22 @@ containerName=git.core.web
 cd /var/jenkins_home/workspace/pipeline/core_web.demo
 echo "当前路径：$PWD"
 
-echo '尝试删除容器'
+echo "尝试删除容器"
 if docker ps -a | grep $imageName ;then
-        echo '存在容器$imageName'
+        echo "存在容器$imageName"
         docker rm -f $imageName
-        echo '删除容器$imageName成功'
+        echo "删除容器$imageName成功"
 fi
 
-echo '尝试删除镜像'
+echo "尝试删除镜像"
 if docker images | grep $containerName ; then
-        echo '存在镜像$containerName'
+        echo "存在镜像$containerName"
         docker rm -f $containerName
-        echo '删除镜像$containerName成功'
+        echo "删除镜像$containerName成功"
 fi
 
-echo '构建镜像'
+echo "构建镜像"
 docker build -t $imageName .
 
-echo '运行容器'
+echo "运行容器"
 docker run -d -p 12701:80 --name $containerName $imageName
