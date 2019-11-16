72fa428aac49876c858c8532be6ec6e2bc1d64d5
wangtp <tp.wong@foxmail.com>
2019-11-16 13:21:57 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ cd /var/jenkins_home/workspace/pipeline/core_web.demo
 echo "当前路径：$PWD"
 
 echo "尝试删除容器"
-if docker ps -a | grep $imageName ;then
-        echo "存在容器$imageName"
-        docker rm -f $imageName
-        echo "删除容器$imageName成功"
+if docker ps -a | grep $containerName ;then
+        echo "存在容器$containerName"
+        docker rm -f $containerName
+        echo "删除容器$containerName成功"
 fi
 
 echo "尝试删除镜像"
-if docker images | grep $containerName ; then
-        echo "存在镜像$containerName"
-        docker rm -f $containerName
-        echo "删除镜像$containerName成功"
+if docker images | grep $imageName ; then
+        echo "存在镜像$imageName"
+        docker rm -f $imageName
+        echo "删除镜像$imageName成功"
 fi
 
 echo "构建镜像"
